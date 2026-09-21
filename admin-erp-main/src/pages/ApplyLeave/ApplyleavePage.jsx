29c8c52201e0adf4288c45b2b819487e88536b6d
Arisha <arishaali0711@gmail.com>
2026-09-21 20:39:23 +0530

    2ndcommit

--- admin-erp-main/src/pages/ApplyLeave/ApplyleavePage.jsx
+++ admin-erp-main/src/pages/ApplyLeave/ApplyleavePage.jsx
@@ -8,6 +8,7 @@ function ApplyLeavePage() {
         <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
+          {/* oi */}
         </Card.Text>
       </Card.Body>
     </Card>
